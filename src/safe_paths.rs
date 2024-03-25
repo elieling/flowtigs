da5599173e6a51df88e096680adb5f7496a96fc6
Ingervo Eliel E O <ebingerv@dx3-523-28534.ad.helsinki.fi>
2024-03-25 17:18:18 +0200

    Iterating with loop instead of recursion

--- src/safe_paths.rs
+++ src/safe_paths.rs
@@ -42,20 +42,83 @@ pub fn safe_paths(path: &str, k: usize, mut meter: Option<&mut MemoryMeter>) -> 
 
     let mut counter = 0;
     for edge in &edges {
-        if counter == total_edges / 2 || counter == total_edges / 4 || counter == total_edges / 10 {
+
+        // Logging
+        if counter == (total_edges / 2) + (total_edges / 3) ||  total_edges / 2 || counter == total_edges / 4 || counter == total_edges / 10 {
             info!("Coumputed {} / {} edges", counter, total_edges);
             if let Some(ref mut meter) = meter {
                 meter.report();
             }
         }
+
+        // Initializing variables
         let first_edge_id = edge.id;
-        let current_edge = edge.clone();
+        // let current_edge = edge.clone();
         let first_weight = edge.weight;
         let excess_flow = edge.weight;
         let mut safe_path: VecDeque<EdgeId> = VecDeque::new();
+        let mut waiting_list: VecDeque<(VecDeque<EdgeId>, Weight)> = VecDeque::new(); // Path, excess flow                  // , first edge id, weight of first edge
+        // let mut waiting_paths: Vec<VecDeque<EdgeId>> = Vec::new();
+        // let empty path: VecDeque<EdgeId> = vec::new();
+        // let mut path_id = 0;
+
+        // Initializing safe paths starting from edge
         safe_path.push_back(edge.id);
-        recursion(safe_path, first_edge_id, current_edge, first_weight, excess_flow, &weight_of_neighbors_of_each_node, &edgelist, &mut safe_edge_paths, &mut extra_weight_of_paths);
-        counter += 1;
+        for next_edge in edgelist[edge.end_node].values() {
+            let weight_from_this_path = weigth_of_all_next_edges - next_edge.weight;
+            if (excess_flow - weight_from_this_path) > 0 {
+                let mut updated_safe_path = safe_path.clone();
+                updated_safe_path.push_back(next_edge.id);
+                            // waiting_paths.push(updated_safe_path);
+                waiting_list.push_back((updated_safe_path, excess_flow - weight_from_this_path));
+                            // path_id += 1;
+            }
+        }
+
+
+        // Iterating through each safe path starting with edge
+        while !waiting_list.is_empty() {
+
+            let (safe_path, excess_flow) = queue.pop_back().expect("Waiting list is empty");
+            let current_edge_id = safe_path.back().expect("Empty safe path");
+
+            // If a cycle has no leakage, stop running
+            if edge.id == current_edge_id && edge.weight == excess_flow {
+                safe_edge_paths.push(safe_path);
+                extra_weight_of_paths.push(excess_flow);
+                continue;
+            }
+
+            let current_edge = edges[current_edge_id];
+
+            // Verify whether the walk can be continued
+            let mut can_continue = false;
+            let weigth_of_all_next_edges = weight_of_neighbors_of_each_node[current_edge.end_node];
+            for next_edge in edgelist[current_edge.end_node].values() {
+                let weight_from_this_path = weigth_of_all_next_edges - next_edge.weight;
+                if (excess_flow - weight_from_this_path) > 0 {
+                    can_continue = true;
+                }
+            }
+
+            // If it can not continue, we store it in the list of safe paths
+            if !can_continue {
+                safe_edge_paths.push(safe_path);
+                extra_weight_of_paths.push(excess_flow);
+            } else {
+                // Extend safe path while keeping positive excess flow
+                for next_edge in edgelist[current_edge.end_node].values() {
+                    let weight_from_this_path = weigth_of_all_next_edges - next_edge.weight;
+                    if (excess_flow - weight_from_this_path) > 0 {
+                        let mut updated_safe_path = safe_path.clone();
+                        updated_safe_path.push_back(next_edge.id);
+                        waiting_list.push_back((updated_safe_path, excess_flow - weight_from_this_path));
+                    }
+                }
+            }
+            // recursion(safe_path, first_edge_id, current_edge, first_weight, excess_flow, &weight_of_neighbors_of_each_node, &edgelist, &mut safe_edge_paths, &mut extra_weight_of_paths);
+            counter += 1;
+        }
     }
 
     info!("Safe paths calculated successfully.");
